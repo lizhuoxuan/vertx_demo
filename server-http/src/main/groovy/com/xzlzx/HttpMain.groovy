07e0ea7a3a949246a48590af71ae39762f1e93a1
lzx <307871680@qq.com>
2019-03-21 16:20:15 +0800

    1

--- server-http/src/main/groovy/com/xzlzx/HttpMain.groovy
+++ server-http/src/main/groovy/com/xzlzx/HttpMain.groovy
@@ -1,12 +1,17 @@
-import groovy.json.JsonOutput
+package com.xzlzx
+
+
+import groovy.util.logging.Slf4j
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.StaticHandler
 
 /**
  * Created by liurui on 2016/11/30.
  */
+@Slf4j
 class HttpMain extends AbstractVerticle {
 
     static void main(String[] args) {
@@ -17,6 +22,14 @@ class HttpMain extends AbstractVerticle {
     void start() throws Exception {
         def router = Router.router(vertx)
 
+        def sharedData = vertx.sharedData()
+        sharedData.getAsyncMap("test_map", {
+            if (it.succeeded()) {
+                println(it.result())
+            } else {
+                println(it.cause().message)
+            }
+        })
         def eb = vertx.eventBus()
         router.get("/test").handler({ ctx ->
             eb.send("gateway", new JsonObject([mess:"123"]), {
@@ -29,14 +42,18 @@ class HttpMain extends AbstractVerticle {
                     ctx.response().end(it.cause().message)
                 }
             })
-            ctx.response().end("haha")
+//            ctx.response().end("haha")
         })
-
-        vertx.createHttpServer().requestHandler(router).listen(8080, {
+        def staticHandler = StaticHandler.create()
+        staticHandler.setAllowRootFileSystemAccess(true)
+        def addr = "D:\\work\\vertx_demo\\server-http\\src\\main\\resources\\dist\\"
+        staticHandler.setWebRoot(addr)
+        router.route().handler(staticHandler)
+        vertx.createHttpServer().requestHandler(router).listen(7788, {
             if (it.succeeded()) {
-                println("success")
+                log.info("success")
             } else {
-                println(it.cause().message)
+                log.info(it.cause().message)
             }
         })
     }
