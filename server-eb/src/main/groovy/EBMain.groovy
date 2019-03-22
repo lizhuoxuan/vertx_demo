ded592b0cce513437c89dff8be67d7a4bbbb34cb
lzx <307871680@qq.com>
2019-03-22 10:35:34 +0800

    1

--- server-eb/src/main/groovy/EBMain.groovy
+++ server-eb/src/main/groovy/EBMain.groovy
@@ -1,5 +1,9 @@
+import io.reactiverse.pgclient.PgClient
+import io.reactiverse.pgclient.PgPool
+import io.reactiverse.pgclient.PgPoolOptions
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.EventBus
 import io.vertx.core.json.JsonObject
 
 /**
@@ -11,16 +15,46 @@ class EBMain extends AbstractVerticle {
         Vertx.vertx().deployVerticle(EBMain.newInstance())
     }
 
+    PgPool pgPool
+    EventBus eb
+
+    def channelName = 'flunk'
+
     @Override
     void start() throws Exception {
-        def eb = vertx.eventBus()
+        eb = vertx.eventBus()
 
-        eb.consumer("gateway", {
-            println(it.body())
-            it.reply(new JsonObject([mess:"i received"]))
+        pgPool = PgClient.pool(vertx, buildPgPoolOptions())
+        // A simple query
+        pgPool.query("SELECT * FROM t_test", { ar ->
+            if (ar.succeeded()) {
+                def result = ar.result()
+                System.out.println("Got " + result.size() + " results ")
+            } else {
+                System.out.println("Failure: " + ar.cause().getMessage())
+            }
+
+            // Now close the pool
+            pgPool.close()
         })
+    }
 
-        println("ok")
+    PgPoolOptions buildPgPoolOptions() {
+        def port = config().getInteger("port", 5432)
+        def host = config().getString("host", "192.168.12.99")
+        def database = config().getString("database", "db_zdsapp")
+        def user = config().getString("user", "zds")
+        def password = config().getString("password", "hubushangdu16")
+        def maxSize = config().getInteger("maxsize", 2)
+
+        new PgPoolOptions()
+                .setPort(port)
+                .setHost(host)
+                .setDatabase(database)
+                .setUser(user)
+                .setPassword(password)
+                .setMaxSize(maxSize)
+                .setCachePreparedStatements(true)
     }
 
 }
